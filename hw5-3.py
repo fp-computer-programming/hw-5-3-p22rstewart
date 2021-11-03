3d7171719ca0a627694532e1860ff08906a88cc8
p22rstewart <p22rstewart@fairfieldprep.org>
2021-11-03 09:56:44 -0400

    .

--- hw5-3.py
+++ hw5-3.py
@@ -3,16 +3,16 @@
 import calendar
 
 # Question 1 
-calendar.TextCalendar().pryear(2020)
+calendar.TextCalendar().pryear(2021)
 
 # Question 2
-calendar.TextCalendar(calendar.SUNDAY).pryear(2020)
+calendar.TextCalendar(6).pryear(2021)
 
 # Question 3
-calendar.TextCalendar().prmonth(2004, 8)
+calendar.TextCalendar().prmonth(2021, 8)
 
 # Question 4
-calendar.LocaleTextCalendar(6, "SPANISH").pryear(2020)
+#calendar.LocaleTextCalendar(6, "SPANISH").pryear(2020)
 
 # Question 5 
 print(calendar.isleap(2004))
